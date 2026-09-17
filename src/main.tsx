38076918890a10ee00e2dde7d85affe66475eec4
arawill <felix12mora@gmail.com>
2026-09-17 08:46:50 +0200

    CAMPAIGN REDESIGN 1A.2

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import '@fontsource/plus-jakarta-sans/500.css'
 import '@fontsource/plus-jakarta-sans/600.css'
 import '@fontsource/plus-jakarta-sans/700.css'
 import './index.css'
-import './styles/normalCampaign.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
